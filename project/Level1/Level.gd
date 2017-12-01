bfba4b22879fc4b994102c61ce38702a432f7102
Samuel Evans-Powell <sam@livebranchstudios.com.au>
2017-12-01 14:15:28 +1100

    WIP

--- project/Level1/Level.gd
+++ project/Level1/Level.gd
@@ -1,6 +1,6 @@
 extends "res://Base/Level.gd"
 
-export var max_rats = 20
+export var max_rats = 2
 onready var primary_rat_id = (randi() % max_rats) + 1
 
 onready var rat_spawner = get_node("SceneVp/Spatial/RatSpawn")
--- project/Level1/State-Victory.gd
+++ project/Level1/State-Victory.gd
@@ -1,5 +1,7 @@
 extends "res://Base/State-Victory.gd"
 
+export(NodePath) var loading_scene = "res://Base/Loading Screen.tscn"
+export(NodePath) var next_scene = "res://Level2/Level2.tscn"
 
 func _ready():
     pass
@@ -7,3 +9,5 @@ func _ready():
 func enter_state(from_state, message):
     .enter_state(from_state, message)
     level._set_message("Well done. Now lets move on to the next phase - the human genome...")
+    
+    get_node("/root/SceneSwitcher").transition_to_scene(next_scene, loading_scene)
