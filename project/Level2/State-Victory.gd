5948205ff064ae50b8c040e1285ca95f31e7ff32
Samuel Evans-Powell <sam@livebranchstudios.com.au>
2017-12-01 14:24:38 +1100

    Level Transitions

--- project/Level2/State-Victory.gd
+++ project/Level2/State-Victory.gd
@@ -1,5 +1,6 @@
 extends "res://Base/State-Victory.gd"
 
+export(NodePath) var next_scene = "res://Credits/Credits.tscn"
 
 func _ready():
     pass
@@ -7,3 +8,5 @@ func _ready():
 func enter_state(from_state, message):
     .enter_state(from_state, message)
     level._set_message("Well done. Now lets move on to the next phase - the human genome...")
+    
+    get_node("/root/SceneSwitcher").cut_to_scene(next_scene)
